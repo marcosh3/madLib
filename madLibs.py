a5a511e1cdf06044fdd24973de0f1475a32f89be
marcosh3 <41239929+marcosh3@users.noreply.github.com>
2021-01-20 18:08:55 -0300

    Initial Commit

---
+++ madLibs.py
@@ -0,0 +1,18 @@
+# engineer = "Marcos H Santos" #some string variable
+# # a few ways to do this
+# print("Hire: " + engineer)
+# print("Hire: {}".format(engineer))
+# print(f"Hire: {engineer}")
+
+adj = input("Adjective: ")
+
+verb1 = input("Verb: ")
+
+verb2 = input("Verb: ")
+
+famous_person = input("Famous person: ")
+
+madlib = f"Electrical Engineering is so {adj}! It makes me so excited all the time because I love to {verb1}. \
+Eat well, make some exercises and {verb2} like you are {famous_person}!"
+
+print(madlib)
